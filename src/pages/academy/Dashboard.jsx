5960cfb95aaf7a6b645af2612144558ad1a7f22e
Tjay05 <tjayolatayo@gmail.com>
2025-02-04 15:16:09 +0100

    Create and add mobile styles for student dashboard

--- src/pages/academy/Dashboard.jsx
+++ src/pages/academy/Dashboard.jsx
@@ -1,7 +1,59 @@
+import CoverImg from "../../assets/images/blue-bg.jpg";
+
 const StudentDashboard = () => {
   return (
     <>
-    
+      <section className="courses">
+        <nav className="studNav">
+          <ul>
+            <li>
+              <h1>New Courses</h1>
+            </li>
+            <li>
+              <p>See all</p>
+            </li>
+          </ul>
+        </nav>
+        <div className="courseSection">
+          <article>
+            <img src={CoverImg} alt="" />
+            <h2>Art in Fashion</h2>
+            <p className="source">Tjay</p>
+          </article>
+          <article>
+            <img src={CoverImg} alt="" />
+            <h2>Detailed Beauty</h2>
+            <p className="source">Tjay</p>
+          </article>
+          <article>
+            <img src={CoverImg} alt="" />
+            <h2>Art in Fashion</h2>
+            <p className="source">Tjay</p>
+          </article>
+        </div>
+      </section>
+      <section className="courses">
+        <nav className="studNav">
+          <ul>
+            <li>
+              <h1>Earn a Certificate</h1>
+            </li>
+            <li>
+              <p>See all</p>
+            </li>
+          </ul>
+        </nav>
+        <div className="courseSection">
+          <article>
+            <img src={CoverImg} alt="" />
+            <h2>Certificate in Art</h2>
+          </article>
+          <article>
+            <img src={CoverImg} alt="" />
+            <h2>Honour in Decoration</h2>
+          </article>
+        </div>
+      </section>
     </>
   );
 }
