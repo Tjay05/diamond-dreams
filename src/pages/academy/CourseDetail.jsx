696d6b8cf6f9acedf29ea29a66e9bd05f5e0e46b
Tjay05 <tjayolatayo@gmail.com>
2025-02-05 13:39:57 +0100

    Add arrows to display modules

--- src/pages/academy/CourseDetail.jsx
+++ src/pages/academy/CourseDetail.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
-import { useLocation, useParams, useNavigate } from "react-router-dom";
-import { IoIosArrowDown } from "react-icons/io"
+import { useParams, useNavigate, Link } from "react-router-dom";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 import back from '../../assets/icons/back-arrow.svg'
+import courses from "../../data/Course";
 
 const CourseDetails = () => {
   const [expandedModule, setExpandedModule] = useState(null);
 
   const history = useNavigate();
-  const { state } = useLocation();
   const { id } = useParams();
 
-  if (!state || !state.course) return <>
+  const course = Object.values(courses).find((c) => c.id === parseInt(id));
+
+  if (!course) return <>
     <img src={back} alt='Back' onClick={() => history(-1)} />
     <h2>Course not Found</h2>
   </>;
@@ -23,25 +25,35 @@ const CourseDetails = () => {
   return (
     <>
       <nav className="btmNav courseDetail">
-        <img src={back} alt='Back' onClick={() => history(-1)} />
-        <h1>{ state.course.title }</h1>
+        <img src={back} alt='Back' onClick={() => history(`/student`)} />
+        <h1>{ course.title }</h1>
       </nav>
       <h2 className="syllabus">Syllabus: What you will learn in this course</h2>
-      {state.course.modules.map((module) => (
+      {course.modules.map((module) => (
       <div className="courseDetails" key={module.id}>
         <li>
           <p>MODULE { module.id }</p> 
           <strong>{module.title}</strong>
-          <IoIosArrowDown 
-            size={20} 
-            color='#5c0002'
-            onClick={() => toggleModule(module.id)} 
-          />
+          {expandedModule === module.id ? (
+            <IoIosArrowUp
+              size={20}
+              color="#5c0002"
+              onClick={() => toggleModule(module.id)} 
+            />
+          ) : (
+            <IoIosArrowDown 
+              size={20} 
+              color='#5c0002'
+              onClick={() => toggleModule(module.id)} 
+            />
+          )}
         </li>
         <ul className={expandedModule === module.id ? `expandedModule`: ``}>
           {module.lessons.map((lesson) => (
             <li key={lesson.id}>
-              <strong>{ lesson.title }</strong> - { lesson.content }
+              <Link to={`module/${module.id}/lesson/${lesson.id}`}>
+                <strong>{ lesson.title }</strong> - { lesson.content }
+              </Link>
             </li>
           ))}
         </ul>
