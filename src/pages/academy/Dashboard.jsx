1de8b35c41a7d04f27563f5ef26f248ce9e0bbde
Tjay05 <tjayolatayo@gmail.com>
2025-02-24 15:07:51 +0100

    Add message

--- src/pages/academy/Dashboard.jsx
+++ src/pages/academy/Dashboard.jsx
@@ -78,7 +78,7 @@ const StudentDashboard = () => {
         {/* Learning Streak */}
         <div className="streak">
           <FaFire className="fire-icon" />
-          <span>You have a {streak}-day learning streak, Good Job!</span>
+          <span>You have a {streak}-day learning streak, you!</span>
         </div>
 
         {/* Enrollment and Progress Overview */}
